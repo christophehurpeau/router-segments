876b5df4fe46477d01ac944d7af4d46ea283b178
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2024-07-30 20:47:43 +0200

    chore: pob update

--- dist/definitions/routes/create.d.ts
+++ dist/definitions/routes/create.d.ts
@@ -1,10 +1,10 @@
-import type { LocalizedPathsRecord } from "../types";
+import type { LocaleType, LocalizedPathsRecord } from "../types";
 import { LocalizedEndRoute } from "./LocalizedEndRoute";
 import { LocalizedSegmentRoute } from "./LocalizedSegmentRoute";
 import { NotLocalizedEndRoute as Route } from "./NotLocalizedEndRoute";
 import { NotLocalizedSegmentRoute } from "./NotLocalizedSegmentRoute";
-export declare const createRoute: <Locales extends string, RouteRef>(path: string, completePath: string, ref: RouteRef) => Route<Locales, RouteRef>;
-export declare const createLocalizedRoute: <Locales extends string, RouteRef>(localizedPathsRecord: LocalizedPathsRecord<Locales>, completeLocalizedPathsRecord: LocalizedPathsRecord<Locales>, ref: RouteRef) => LocalizedEndRoute<Locales, RouteRef>;
-export declare const createSegmentRoute: <Locales extends string, RouteRef>(path: string, completePath: string) => NotLocalizedSegmentRoute<Locales, RouteRef>;
-export declare const createLocalizedSegmentRoute: <Locales extends string, RouteRef>(localizedPathsRecord: LocalizedPathsRecord<Locales>, completeLocalizedPathsRecord: LocalizedPathsRecord<Locales>) => LocalizedSegmentRoute<Locales, RouteRef>;
+export declare const createRoute: <Locales extends LocaleType, RouteRef>(path: string, completePath: string, ref: RouteRef) => Route<Locales, RouteRef>;
+export declare const createLocalizedRoute: <Locales extends LocaleType, RouteRef>(localizedPathsRecord: LocalizedPathsRecord<Locales>, completeLocalizedPathsRecord: LocalizedPathsRecord<Locales>, ref: RouteRef) => LocalizedEndRoute<Locales, RouteRef>;
+export declare const createSegmentRoute: <Locales extends LocaleType, RouteRef>(path: string, completePath: string) => NotLocalizedSegmentRoute<Locales, RouteRef>;
+export declare const createLocalizedSegmentRoute: <Locales extends LocaleType, RouteRef>(localizedPathsRecord: LocalizedPathsRecord<Locales>, completeLocalizedPathsRecord: LocalizedPathsRecord<Locales>) => LocalizedSegmentRoute<Locales, RouteRef>;
 //# sourceMappingURL=create.d.ts.map

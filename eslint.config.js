e9ae9e341488ddd64529096f6ff87ca48abb64b4
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2026-08-15 21:09:56 +0200

    chore: update dev dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import pobConfig from "@pob/eslint-config";
 
 export default [
-  ...pobConfig(import.meta.url).configs.node,
+  ...pobConfig.configs.node,
   {
     ignores: ["vite.config.ts"],
   },
